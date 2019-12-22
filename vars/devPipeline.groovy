48b9c82b6706e61f697ba0be65626ce3da82a98d
sgakkoju1 <47368393+sgakkoju1@users.noreply.github.com>
2019-12-22 07:07:53 -0600

    Update devPipeline.groovy

--- vars/devPipeline.groovy
+++ vars/devPipeline.groovy
@@ -1,17 +1,22 @@
 #!/usr/local/bin groovy
 
-stage('Build') {
-            steps {
-                script {
-                echo "Build Stage"
-                }
-            }
-        }
+pipeline {
+            agent any
+            stages {
+                        stage('Build') {
+                                    steps {
+                                    script {
+                                    echo "Build Stage"
+                                    }
+                                    }
+                        }
         
-        stage('Test') {
-            steps {
-                script {
-                echo "Test Stage"
-                }
+                        stage('Test') {
+                                    steps {
+                                    script {
+                                    echo "Test Stage"
+                                    }
+                                    }
+                        }
             }
-        }
+}
